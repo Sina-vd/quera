6e6c8838a68db450dff5bd13c09fcb341a70b4f4
sina rezaie <sina.rz1384@gmail.com>
2025-04-09 20:44:22 +0330

    Add Phase 2

--- sina_rezaii_quera_maktab130.py
+++ sina_rezaii_quera_maktab130.py
@@ -2,14 +2,21 @@ class divar:
     def __init__(self):
          self.users = set()
          self.advertises = {}
+         self.favorites = {}
  
+
+
     def register(self, username):
          if username in self.users:
             print("invalid username")
          else:
             self.users.add(username)
             self.advertises[username] = []
+            self.favorites[username] = []
             print(f"registered successfully : {self.users}")
+
+
+
 
     def add_advertise(self, username, title):
         if username not in self.users:
@@ -27,7 +34,10 @@ class divar:
             else:
                 self.advertises[username].append(title)
                 print(f"posted successfully : {title}")
- 
+
+
+
+
     def rem_advertise(self, username, title):
         if username not in self.users:
             print("invalid username")
@@ -37,6 +47,9 @@ class divar:
             self.advertises[username].remove(title)
             print("removed successfully")
  
+
+
+
     def list_my_advertises(self, username):
         if username not in self.users:
             print("invalid username")
@@ -45,6 +58,47 @@ class divar:
                 print(" ".join(self.advertises[username]))
             else:
                 print("No adverts posted yet.")
+
+
+
+    def add_favorite(self, username, title):
+        if username not in self.users:
+            print("invalid username")
+        found = False
+        for q in self.advertises.values():
+            if title in q:
+                found = True
+                break
+        if not found:
+            print("invalid title")
+        elif title in self.favorites[username]:
+            print("already favorite")
+        else:
+            self.favorites[username].append(title)
+            print(f"added successfully")
+
+
+
+    def rem_favorite(self, username, title):
+        if username not in self.users:
+            print("invalid username")
+        elif title not in self.favorites[username]:
+            print("already not favorite")
+        else:
+            self.favorites[username].remove(title)
+            print("removed successfully")
+
+
+
+    def list_favorite_advertises(self, username):
+        if username not in self.users:
+            print("invalid username")
+        else:
+            if self.favorites[username]:
+                print(" ".join(self.favorites[username]))
+            else:
+                print("no favorite add yet.")
+                
  
 
 divvar = divar()
@@ -66,3 +120,15 @@ print("=="*10)
 divvar.rem_advertise("ali","car")
 print("=="*10)
 divvar.list_my_advertises("ali")
+print("=="*10)
+divvar.add_favorite("sina", "car")
+
+divvar.add_favorite("sina", "mobile")
+
+divvar.add_favorite("ali", "pc")
+
+divvar.add_favorite("ali", "phon")
+
+divvar.list_favorite_advertises("ali")
+
+divvar.list_favorite_advertises("ali")
